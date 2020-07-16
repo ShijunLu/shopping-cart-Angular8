a9faf5e5c8c7a9dadf99155d77e9108c99f13def
ShijunLu <lshijun0000@ucla.edu>
2020-07-16 12:21:55 -0700

    Product List UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/shared/header/header.component';
+import { FooterComponent } from './component/shared/footer/footer.component';
+import { ShoppingCartComponent } from './component/shopping-cart/shopping-cart.component';
+import { NavComponent } from './component/shared/nav/nav.component';
+import { CartComponent } from './component/shopping-cart/cart/cart.component';
+import { FiltersComponent } from './component/shopping-cart/filters/filters.component';
+import { ProductListComponent } from './component/shopping-cart/product-list/product-list.component';
+import { CartItemComponent } from './component/shopping-cart/cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './component/shopping-cart/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ShoppingCartComponent,
+    NavComponent,
+    CartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartItemComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule
